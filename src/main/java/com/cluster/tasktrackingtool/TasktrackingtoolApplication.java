cf013845fc3c2ca11b1884574c03db4abfe95adf
0Cluster <vinaydalalpnp123321@gmail.com>
2024-10-28 15:51:01 +0530

    intial commit

---
+++ src/main/java/com/cluster/tasktrackingtool/TasktrackingtoolApplication.java
@@ -0,0 +1,13 @@
+package com.cluster.tasktrackingtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasktrackingtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasktrackingtoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cluster/tasktrackingtool/TasktrackingtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cluster.tasktrackingtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasktrackingtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
